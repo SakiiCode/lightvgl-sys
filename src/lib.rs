61b3b2455f5a5f04548ebff5a6a8fde3e20d4934
Sakii <13037299+SakiiCode@users.noreply.github.com>
2025-06-22 18:56:07 +0200

    export bindings code as str

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+#![no_std]
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub fn _bindgen_raw_src() -> &'static str {
+    include_str!(concat!(env!("OUT_DIR"), "/bindings.rs"))
+}
